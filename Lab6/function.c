ad16d7da185f3c5d071d460af709d17ce6abedf1
6174gh <6174gh@gmail.com>
2022-11-04 12:44:02 +0530

    04112022

--- Lab6/function.c
+++ Lab6/function.c
@@ -5,11 +5,12 @@ int main()
     int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int b[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int c[10];
-#pragma omp parallel num_threads(5)
+    omp_set_num_threads(4);
+#pragma omp prallel for
     for (int i = 0; i < 0; i++)
     {
         c[i] = a[i] + b[i];
     }
-    printf("Threads are set using directive clause\n");
+    printf("Threads are set using omp_set_num_threads()\n");
     return 0;
 }
